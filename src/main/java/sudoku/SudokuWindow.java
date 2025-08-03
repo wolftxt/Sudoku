fe918b0268009f5ac21f569daaea98c8a3e1b9d0
wolftxt <wolfdavid49@gmail.com>
2025-08-03 15:49:00 +0200

    Added a menu bar

--- src/main/java/sudoku/SudokuWindow.java
+++ src/main/java/sudoku/SudokuWindow.java
@@ -2,6 +2,9 @@ package sudoku;
 
 import com.formdev.flatlaf.FlatDarkLaf;
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 import javax.swing.WindowConstants;
 
 public class SudokuWindow extends javax.swing.JFrame {
@@ -16,8 +19,30 @@ public class SudokuWindow extends javax.swing.JFrame {
     private void initComponents() {
 
         widget = new SudokuWidget();
+
         this.setLayout(new BorderLayout());
+
         this.add(widget, BorderLayout.CENTER);
+
+        JMenuBar menuBar = new JMenuBar();
+
+        JMenu newGame = new JMenu("New Game");
+        newGame.addActionListener(e -> widget.newGame());
+        menuBar.add(newGame);
+
+        JMenu hint = new JMenu("Hint");
+        hint.addActionListener(e -> {
+            throw new RuntimeException("NOT IMPLEMENTED");
+        });
+        menuBar.add(hint);
+
+        JMenu solve = new JMenu("Solve");
+        solve.addActionListener(e -> {
+            throw new RuntimeException("NOT IMPLEMENTED");
+        });
+        menuBar.add(solve);
+
+        this.setJMenuBar(menuBar);
     }
 
     public static void main(String args[]) {
