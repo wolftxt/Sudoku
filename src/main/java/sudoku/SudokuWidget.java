49946634441a7df1c61a350a1bcc539b618051bf
wolftxt <wolfdavid49@gmail.com>
2025-08-03 14:08:09 +0200

    Added number visualization

--- src/main/java/sudoku/SudokuWidget.java
+++ src/main/java/sudoku/SudokuWidget.java
@@ -1,12 +1,14 @@
 package sudoku;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import javax.swing.JComponent;
 
 public class SudokuWidget extends JComponent {
 
     private static final Color GRID = Color.BLACK;
+    private static final Color[] COLORS = new Color[]{new Color(0, 0, 0, 0), Color.RED, Color.ORANGE, Color.YELLOW, Color.GREEN, Color.CYAN, Color.BLUE, Color.MAGENTA, Color.PINK, new Color(255, 128, 0)};
 
     private SudokuGame game;
 
@@ -31,11 +33,22 @@ public class SudokuWidget extends JComponent {
         int[][] board = game.getBoard();
 
         int s = getScaling(SIZE, SIZE);
-        // Draw background
+        // Draw grid
         g.setColor(GRID);
         for (int x = 0; x < SIZE; x++) {
             for (int y = 0; y < SIZE; y++) {
                 g.drawRect(x * s, y * s, s, s);
+            }
+        }
+        // Draw numbers
+        g.setFont(new Font("SudokuFont", Font.BOLD, s * 2 / 3));
+        for (int x = 0; x < SIZE; x++) {
+            for (int y = 0; y < SIZE; y++) {
+                int num = board[x][y];
+                g.setColor(COLORS[num]);
+                int xStart = x * s + s / 4; // adding s / 4 centralises the numbers
+                int yStart = (y + 1) * s - s / 4; // + 1 because strings are drawn in the top right direction
+                g.drawString(Integer.toString(num), xStart, yStart);
             }
         }
     }
