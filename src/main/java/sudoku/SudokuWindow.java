5769eed2f887d683b64bf7ccfc3c463e82bc8e06
wolftxt <wolfdavid49@gmail.com>
2025-08-03 18:01:38 +0200

    Added a way to faster set numbers by dragging

--- src/main/java/sudoku/SudokuWindow.java
+++ src/main/java/sudoku/SudokuWindow.java
@@ -6,6 +6,7 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import javax.swing.JMenu;
@@ -14,7 +15,7 @@ import javax.swing.JMenuItem;
 import javax.swing.WindowConstants;
 
 public class SudokuWindow extends javax.swing.JFrame {
-    
+
     public SudokuWindow() {
         this.setTitle("Sudoku");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -26,9 +27,15 @@ public class SudokuWindow extends javax.swing.JFrame {
             }
         });
     }
-    
+
     private void initComponents() {
         widget = new SudokuWidget();
+        widget.addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                widget.click(e.getX(), e.getY());
+            }
+        });
         widget.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseReleased(MouseEvent e) {
@@ -44,38 +51,38 @@ public class SudokuWindow extends javax.swing.JFrame {
             }
         });
         this.setLayout(new BorderLayout());
-        
+
         this.add(widget, BorderLayout.CENTER);
-        
+
         JMenuBar menuBar = new JMenuBar();
         JMenu game = new JMenu("Game");
-        
+
         JMenuItem newGame = new JMenuItem("New Game");
         newGame.addActionListener(e -> widget.newGame());
         game.add(newGame);
-        
+
         game.addSeparator();
-        
+
         JMenuItem hint = new JMenuItem("Hint");
         hint.addActionListener(e -> {
             System.out.println("NOT IMPLEMENTED");
         });
         game.add(hint);
-        
+
         JMenuItem solve = new JMenuItem("Solve");
         solve.addActionListener(e -> {
             System.out.println("NOT IMPLEMENTED");
         });
         game.add(solve);
-        
+
         menuBar.add(game);
         this.setJMenuBar(menuBar);
     }
-    
+
     public static void main(String args[]) {
         FlatDarkLaf.setup();
         java.awt.EventQueue.invokeLater(() -> new SudokuWindow().setVisible(true));
     }
-    
+
     private SudokuWidget widget;
 }
