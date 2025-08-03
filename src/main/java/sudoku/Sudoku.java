09f5d04af186eab5e10a0b949bb0d862403f6f66
wolftxt <wolfdavid49@gmail.com>
2025-08-03 11:19:52 +0200

    Initial project creation commit

---
+++ src/main/java/sudoku/Sudoku.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package sudoku;
+
+/**
+ *
+ * @author davidwolf
+ */
+public class Sudoku {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
